31bde97e1f93279b89b9f68e42b24c383bf953d0
keadtijarupat github <keadtijarupat.developers@gmail.com>
2020-11-08 14:13:51 +0700

    gin log

--- Golang/index.go
+++ Golang/index.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"rest/api"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +11,30 @@ import (
 func main() {
 
 	router := gin.Default()
+	runningDir, _ := os.Getwd()
+	errlogfile, _ := os.OpenFile(fmt.Sprintf("%s/gin_error.log", runningDir), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	accesslogfile, _ := os.OpenFile(fmt.Sprintf("%s/gin_access.log", runningDir), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+
+	gin.DefaultErrorWriter = errlogfile
+	gin.DefaultWriter = accesslogfile
+
+	// router.Use(gin.Logger())
+	// custom format logger
+
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		return fmt.Sprintf("%s  \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+
 	router.Static("/uploads", "../volumes/uploads")
 
 	api.Setup(router)
